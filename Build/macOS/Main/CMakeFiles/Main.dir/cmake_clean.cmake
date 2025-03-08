6e3b80c565f5beb884dd6b01659524653f5c8d7f
woodgh <woodgh@naver.com>
2025-03-08 12:07:29 +0900

    Added: Makefile 프로젝트 추가

---
+++ Build/macOS/Main/CMakeFiles/Main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Main.dir/Main.cpp.o"
+  "CMakeFiles/Main.dir/Main.cpp.o.d"
+  "libMain.a"
+  "libMain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
